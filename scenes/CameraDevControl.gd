b36a9ef08ff6d805550535395424839bfc9933b6
Kaden Jackson <winners769@gmail.com>
2023-09-02 20:05:34 -0400

    quick sneak addd

--- scenes/CameraDevControl.gd
+++ scenes/CameraDevControl.gd
@@ -6,8 +6,11 @@ func _input(event):
 	if event.is_action_pressed("CameraControl"):
 		if zoomed == true:
 			zoom = Vector2(1,1)
+			
 			zoomed = false
+			get_parent().maxSpeed = 100
 		else:
 			zoom = Vector2(3,3)
 			zoomed = true
-		
+			get_parent().maxSpeed = 300
+		print(zoomed)
