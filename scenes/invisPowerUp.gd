99c5498bbf31ebd015d9ab8c1677ccfb036aca86
KadenJ <72470602+KadenJ@users.noreply.github.com>
2024-08-29 23:11:01 -0400

    fixed window scaling and cleaned code

--- scenes/invisPowerUp.gd
+++ scenes/invisPowerUp.gd
@@ -4,17 +4,17 @@ extends Area2D
 var player
 @export var powerTime : float
 
-
 func _on_body_entered(body):
+	if player == null:
+		player = body
+	$CollisionShape2D.set_deferred("disabled", true)
 	hide()
-	$CollisionShape2D.disabled = true
-	player = body
+	
 	
 	player.set_collision_layer_value(4, false)
 	player.set_modulate("ffffff5a")
 	$powerTimer.start(powerTime)
 	#set wait time of timer and start
-
 
 func _on_timer_timeout():
 	player.set_collision_layer_value(4, true)
