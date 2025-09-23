7cbd55785b7cb172216ce0ccbb481f0889b54323
KadenJacks0n <90848696+KadenJacks0n@users.noreply.github.com>
2025-09-22 23:52:55 -0400

    rolled back onine script
    Lootlocker asset was having trouble on mobile, rolled back to pure single script for connection

--- LootLockerScores.gd
+++ LootLockerScores.gd
@@ -9,9 +9,12 @@ var game_API_key = "prod_315f351de7474f4b92dd307c55825de5"
 var development_mode = false
 var leaderboard_key = "Live"
 var session_token = ""
-var player_identifier = ""
+var player_identifier = OS.get_unique_id()
 #var score = 0
 
+var auth_http = HTTPRequest.new()
+var leaderboard_http = HTTPRequest.new()
+var submit_score_http = HTTPRequest.new()
 
 signal toggleWifi
 signal gotLeaderboard
@@ -19,74 +22,123 @@ signal gotLeaderboard
 func _ready():
 	loadScores()
 
-
 func _authentication_request():
 	# Check if a player session exists
-	@warning_ignore("unused_variable")
 	var player_session_exists = false
+	var player_identifier : String
 	var file = FileAccess.open("user://LootLocker.data", FileAccess.READ)
 	if file != null:
 		player_identifier = file.get_as_text()
 		#print("player ID="+player_identifier)
 		file.close()
- 
+
 	if player_identifier != null and player_identifier.length() > 1:
 		#print("player session exists, id="+player_identifier)
 		player_session_exists = true
 	if(player_identifier.length() > 1):
 		player_session_exists = true
-		
-	
-	var guestLoginResponse = await LL_Authentication.GuestSession.new(player_identifier).send()
-	if(!guestLoginResponse.success):
+
+	## Convert data to json string:
+	var data = { "game_key": game_API_key, "game_version": "0.0.0.1", "development_mode": true }
+
+	# Add 'Content-Type' header:
+	var headers = ["Content-Type: application/json"]
+
+	# Create a HTTPRequest node for authentication
+	auth_http = HTTPRequest.new()
+	add_child(auth_http)
+	auth_http.request_completed.connect(_on_authentication_request_completed)
+	# Send request
+	auth_http.request("https://api.lootlocker.io/game/v2/session/guest", headers, HTTPClient.METHOD_POST, JSON.stringify(data))
+
+func _on_authentication_request_completed(result, response_code, headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		print("offline")
 		online = false
-		printerr("Guest login failed with reason: ")
-		toggleWifi.emit()
-		return
+	else:
+		online = true
+		var json = JSON.new()
+		json.parse(body.get_string_from_utf8())
 		
-	print("Guest user was successfully signed in to LootLocker")
-
-	online = true
+		# Save the player_identifier to file
+		var file = FileAccess.open("user://LootLocker.data", FileAccess.WRITE)
+		file.store_string(json.get_data().player_identifier)
+		file.close()
+		
+		# Save session_token to memory
+		session_token = json.get_data().session_token
+		
+		
+		# Clear node
+		auth_http.queue_free()
+		# Get leaderboards
+		#_get_leaderboards()
+		online = true
 	toggleWifi.emit()
-	
-
-
 
 func _get_leaderboards():
 	#leaderboard needs to load scores before writing label
-	
-	var count : int = 6;
+	#print("Getting leaderboards")
+	var url = "https://api.lootlocker.io/game/leaderboards/"+leaderboard_key+"/list?count=10"
+	var headers = ["Content-Type: application/json", "x-session-token:"+session_token]
 
-	var response = await LL_Leaderboards.GetScoreList.new(leaderboard_key, count).send()
-	if(!response.success) :
-		# Request failed, handle errors
-		pass
-	else:
-	# Request succeeded, use response as applicable in your game logic
-		var top = []
-		for item in response['items']:
-			top.append(item["score"])
-		OnlineTopScores.clear()
-		OnlineTopScores.append_array(top)
+	# Create a request node for getting the highscore
+	leaderboard_http = HTTPRequest.new()
+	add_child(leaderboard_http)
+	leaderboard_http.request_completed.connect(_on_leaderboard_request_completed)
+
+	# Send request
+	leaderboard_http.request(url, headers, HTTPClient.METHOD_GET, "")
+
+
+func _on_leaderboard_request_completed(result, response_code, headers, body):
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+
+
+
+	var counter = 0
+	print(json.get_data())
+	for n in json.get_data().items.size():
+		OnlineTopScores[counter] = json.get_data().items[n].score
+		counter +=1
+		if counter == len(OnlineTopScores):
+			OnlineTopScores.sort()
+			OnlineTopScores.reverse()
+			break
 		gotLeaderboard.emit()
-	print(OnlineTopScores)
-	
+	Events.gotScores.emit()
+	# Print the formatted leaderboard to the console
+	#print(OnlineTopScores)
 
-
-
+	# Clear node
+	#leaderboard_http.queue_free()
 
 #bad request on upload
 func _upload_score(score: int):
 	#can't be same member ID
-	
-	var response = await LL_Leaderboards.SubmitScore.new(leaderboard_key, score, player_identifier).send()
-	if(!response.success) :
-	# Request failed, handle errors
-		pass
-	else:
-	# Request succeeded, use response as applicable in your game logic
-		print(response)
-	
+	var data = { "member_id": str(randi_range(0,10000)).pad_zeros(5), "score": str(score)}
+	var url : String = "https://api.lootlocker.io/game/leaderboards/"+leaderboard_key+"/submit"
+	var headers = ["Content-Type: application/json", "x-session-token:"+session_token]
+	submit_score_http = HTTPRequest.new()
+	add_child(submit_score_http)
+	submit_score_http.request_completed.connect(_on_upload_score_request_completed)
+	# Send request
+	submit_score_http.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(data))
+	# Print what we're sending, for debugging purposes:
+
+
+
+func _on_upload_score_request_completed(result, response_code, headers, body) :
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+
+	# Print data
+	print(json.get_data())
+
+	#print(response_code)
+	# Clear node
+	submit_score_http.queue_free()
 
 
 #offline#########################################################################################
